62c2aa36aa422459de54e2e2f7a53c54c497f364
MahmoudElSayed96 <gcfjxvkj@gmail.com>
2024-08-13 06:47:51 +0300

    intialize shardpref and sqflite and handle window screen options

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,37 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:window_manager/window_manager.dart';
 
 import 'app.dart';
+import 'database/sql.dart';
+import 'utils/helpers/shared_pref.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await CacheData.cacheDataInit();
+  sqfliteFfiInit();
+  databaseFactory = databaseFactoryFfi;
+  if (Platform.isWindows) {
+    await windowManager.ensureInitialized();
+
+    WindowOptions windowOptions = const WindowOptions(
+      size: Size(1280, 800),
+      center: true,
+      backgroundColor: Colors.transparent,
+      title: 'Slice Master Pro',
+      titleBarStyle: TitleBarStyle.normal,
+    );
+
+    windowManager.waitUntilReadyToShow(windowOptions, () async {
+      await windowManager.show();
+      await windowManager.focus();
+      await windowManager.setResizable(true);
+      await windowManager.setMinimumSize(const Size(1024, 768));
+    });
+  }
+  await SqlDb().initializeDatabase();
+
   runApp(const MyApp());
 }
