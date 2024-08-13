e8d6add975ea7405eea9b13ba30ec23ffa9c8aba
MahmoudElSayed96 <gcfjxvkj@gmail.com>
2024-08-13 06:45:57 +0300

    create customSnackBar

---
+++ lib/utils/helpers/my_snackbar.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:slice_master_pro/utils/extentions/extentions.dart';
+
+import '../constants/colors.dart';
+
+void customSnackBar(BuildContext context,
+    [String? message, Color? color, int? seconds]) {
+  ScaffoldMessenger.of(context).hideCurrentSnackBar();
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      width: context.width / 3,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(14)),
+      duration: Duration(seconds: seconds ?? 2),
+      backgroundColor: (color ?? ColorManager.orange).withOpacity(0.9),
+      behavior: SnackBarBehavior.floating,
+      content: Center(
+        child: Text(message ?? "there was an error please try again later!",
+            style: context.textTheme.displaySmall),
+      )));
+}
