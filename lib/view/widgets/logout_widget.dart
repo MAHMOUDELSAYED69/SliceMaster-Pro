5e0270de8ba485fc79902c58700ba62b3f3f0e16
MahmoudElSayed96 <gcfjxvkj@gmail.com>
2024-08-14 22:27:02 +0300

    create LogoutWidget

---
+++ lib/view/widgets/logout_widget.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../utils/constants/colors.dart';
+import '../../utils/constants/routes.dart';
+import '../../utils/helpers/my_snackbar.dart';
+import '../../viewmodel/logout/logout_cubit.dart';
+
+class LogoutWidget extends StatelessWidget {
+  const LogoutWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Tooltip(
+      margin: const EdgeInsets.only(top: 5),
+      height: 34,
+      message: 'Logout',
+      decoration: BoxDecoration(
+          color: ColorManager.red, borderRadius: BorderRadius.circular(4)),
+      child: BlocListener<AuthStatus, AuthStatusState>(
+        listener: (context, state) {
+          if (state is Logout) {
+            Navigator.pushNamedAndRemoveUntil(
+                context, RouteManager.login, (route) => false);
+            customSnackBar(context, 'Logout Successfully!');
+          }
+
+          if (state is LogoutFailure) {
+            customSnackBar(context, 'There was an error!');
+          }
+        },
+        child: IconButton(
+          icon: const Icon(Icons.logout),
+          onPressed: () => context.read<AuthStatus>().logout(),
+        ),
+      ),
+    );
+  }
+}
